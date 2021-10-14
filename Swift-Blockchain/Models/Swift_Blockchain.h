91f47643731a79d64f6d61c7e09d9124d47afb6d
Arseni <arsenii12345@yandex.ru>
2021-10-14 16:23:35 +0300

    Fix some func

---
+++ Swift-Blockchain/Models/Swift_Blockchain.h
@@ -0,0 +1,18 @@
+//
+//  Swift_Blockchain.h
+//  Swift-Blockchain
+//
+//  Created by Arseni Laputska on 14.10.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swift_Blockchain.
+FOUNDATION_EXPORT double Swift_BlockchainVersionNumber;
+
+//! Project version string for Swift_Blockchain.
+FOUNDATION_EXPORT const unsigned char Swift_BlockchainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Blockchain/PublicHeader.h>
+
+
